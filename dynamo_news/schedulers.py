6142c83b0b53122532650fa9944aee087584f5b9
Hamim <41739275+seracoder@users.noreply.github.com>
2025-08-14 12:08:22 +0600

    Delete old forex news-related code

--- dynamo_news/schedulers.py
+++
@@ -1,9 +0,0 @@
-from apscheduler.job import Job
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
-from bot.config import Var
-
-schedule = AsyncIOScheduler()
-job_defaults = {"coalesce": False, "max_instances": 1}
-schedule.configure(timezone=Var.TZ, job_defaults=job_defaults)
-schedule_ids: list[Job] = []
